5de5144a9877e73a269d6383b7f338f86936dd44
Busari <mudicnoni@gmail.com>
2024-01-18 09:24:32 -0800

    all tasks

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
 
-"""
-magic_calculation - Replicates the behavior of given Python bytecode
-"""
-
 def magic_calculation(a, b):
-        add, sub = __import__('magic_calculation_102', globals(), locals(), ['add', 'sub']).add, __import__('magic_calculation_102', globals(), locals(), ['add', 'sub']).sub
-            
+        """Match bytecode provided"""
+            from magic_calculation_102 import add, sub
+
                 if a < b:
                             c = add(a, b)
-                                    
-                                            for i in range(4, 6):
-                                                            c = add(c, i)
-                                                                    
-                                                                            return c
-                                                                            else:
-                                                                                        return sub(a, b)
+                                    for i in range(4, 6):
+                                                    c = add(c, i)
+                                                            return (c)
 
-                                                                                    if __name__ == "__main__":
-                                                                                            pass  # Add test cases or additional functionality if needed
-
+                                                            else:
+                                                                        return (sub(a, b))
